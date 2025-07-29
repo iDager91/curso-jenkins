ec1cba1ea2400e3f7c9c272734cc3d7e070a8153
unknown <idager@live.com>
2025-07-29 10:14:58 -0600

    corrigiendo apso anterior

--- proyecto_pytest/run_tests.sh
+++ proyecto_pytest/run_tests.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
+echo "Iniciando ejecucion de pruebas en Jenkins..." 
 
+#verificar si el entorno virutal existe
 echo "activando el entorno virtual"
-if [!"-d env" ]; then
-    python3 -m venv env
+if [!"-d "venv" ]; then
+    echo "Entorno virtual no encontrado. Creandolo..."
+    python3 -m venv venv
 fi
-source env/bin/activate
 
+#Activar el entorno virtual correctamente
+if [ -f "venv/bin/activate" ]; then
+    source env/bin/activate
+elif [ -f "venv/Scripts/activate" ]; then #Para Windows
+    source venv/Scripts/activate
+else
+    echo "Error: No se pudo acitvar el entorni virtual."
+    exit 1
+fi
+
+#VErificar si pip esta instalado correctamente
 echo "instalando dependencias"
 pip install --upgrade pip
 pip install -r requirements.txt
 
-echo "ejecutando pruevas con pytest"
-pytest tests/ --junitxml=reports/test-results.xml --html=reports/test-results.html --self-contained-html
+#Ejecutar las pruebas
+echo "Ejecutando pruebas con pytest..."
+venv/bin/python -m pytest tests/ --junitxml=reports/test-results.xml --html=reports/test-results.html --self-contained-html
 
 echo "pruebas finalizadas resultados en reports"
