82345deb0fd0bddaedb018a2d3cca3b64a89d744
unknown <idager@live.com>
2025-07-29 10:02:53 -0600

    corrigiendo apso anterior

--- proyecto_pytest/run_tests.sh
+++ proyecto_pytest/run_tests.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 echo "activando el entorno virtual"
+if [!"-d env" ]; then
+    python3 -m venv env
+fi
 source env/bin/activate
 
 echo "instalando dependencias"
+pip install --upgrade pip
 pip install -r requirements.txt
 
 echo "ejecutando pruevas con pytest"
