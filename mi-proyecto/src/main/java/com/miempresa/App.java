1466e0325ac5b1342ac83ccd117af3e924b3c9cb
unknown <idager@live.com>
2025-07-31 17:32:46 -0600

    Primer Proyecto Maven con Jenkins

---
+++ mi-proyecto/src/main/java/com/miempresa/App.java
@@ -0,0 +1,17 @@
+package com.miempresa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! with MAven" );
+    }
+    public int sumar(int a, int b){
+        return a+b;
+        
+    }
+}
--- proyecto_pytest/run_tests.sh
+++ proyecto_pytest/run_tests.sh
@@ -17,3 +17,20 @@ echo "Ejecutando pruebas con pytest..."
 venv/bin/python -m pytest tests/ --junitxml=reports/test-results.xml --html=reports/test-results.html --self-contained-html
 
 echo "Pruebas finalizadas. Resultados en reports"
+
+# Solo necesitas ajustar la configuración del paso “Publish HTML reports” en Jenkins:
+
+# En la sección "HTML directory to archive", cambia la ruta de:
+
+# reports
+# a:
+
+# proyecto_pytest/reports
+# De esta forma, Jenkins bajará al subdirectorio donde realmente se encuentra el archivo HTML generado por Pytest.
+
+# 💡 Bonus Tip
+# Si quieres evitar problemas similares en el futuro, podrías modificar el script para guardar los reportes directamente en la raíz del workspace:
+
+# bash
+# mkdir -p ../reports
+# venv/bin/python -m pytest tests/ --junitxml=../reports/test-results.xml --html=../reports/test-results.h
